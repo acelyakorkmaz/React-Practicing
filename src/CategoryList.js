799fe7bfbf4c5b409c28e97a728b5e8a535120c2
acelyakorkmaz <acelyakorkmaz61@gmail.com>
2023-02-21 20:24:52 +0300

    Events

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -1,32 +1,35 @@
 import React, { Component } from 'react'
-import {ListGroup,ListGroupItem} from 'reactstrap'
+import { ListGroup, ListGroupItem } from 'reactstrap'
 
 export default class CategoryList extends Component {
-  //  constructor(props){
-  //   super(props); 
-  
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [
+        { categoryId: 1, categoryName: "Baverages" },
+        { categoryId: 2, categoryName: "Condiments" },],
+        currentCategory:""
+      }
+  }
+  changeCategory=(category) =>{//fonksiyon
+    this.setState({currentCategory:category.categoryName})
+  }
   render() {
     return (
       <div>
         <h3>{this.props.info.title}</h3>
+        <h3>{this.state.counter}</h3>
         <ListGroup>
-          <ListGroupItem>
-            Cras justo odio
-          </ListGroupItem>
-          <ListGroupItem >
-            Dapibus ac facilisis in
-          </ListGroupItem>
-          <ListGroupItem>
-            Morbi leo risus
-          </ListGroupItem>
-          <ListGroupItem>
-            Porta ac consectetur ac
-          </ListGroupItem>
-          <ListGroupItem>
-            Vestibulum at eros
-          </ListGroupItem>
+          {
+            this.state.categories.map(category => (
+            <ListGroupItem onClick={()=>this.changeCategory(category)} //event set state
+            key={category.categoryId}>{category.categoryName}</ListGroupItem>//performans için her bir elemana id ver sanal domda daha hızlı yakalanır
+            ))//categorie gez her biri için list item oluştur
+          }
+
+
         </ListGroup>
+        <h4>{this.state.currentCategory}</h4>
       </div>
     )
   }
