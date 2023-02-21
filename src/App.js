94ec86211365bd3e02861d0893c6efc51bf1cb07
acelyakorkmaz <acelyakorkmaz61@gmail.com>
2023-02-21 17:47:16 +0300

    bootstrap-reactstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-
-
 import React from "react";
 import CategoryList from "./CategoryList";
 import Navi from "./Navi";
-import ProduckList from "./ProduckList";
+import ProductList from "./ProductList";
+import { Container, Row, Col } from "reactstrap";
 function App() {
   return (
     <div className="App">
-     <Navi/>
-     <CategoryList/>
-     <ProduckList/>
+      <Container>
+        <Row>
+          <Navi />
+        </Row>//bir rowu 12 parçaya ayırıyor
+        <Row>
+          <Col xs="3">
+            <CategoryList />
+          </Col>
+          <Col xs="9">
+            <ProductList /></Col>
+
+        </Row>
+
+      </Container>
+
+
     </div>
   );
 }
